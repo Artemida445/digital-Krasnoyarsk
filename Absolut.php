225416321222d64632e1ae4e255d2ef4bb2cf363
Artem <tikhonov.art@mail.ru>
2020-02-11 14:22:02 +0700

    forms pati

---
+++ Absolut.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Absolut</title>
+</head>
+<body>
+	<form action="OOP.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="pole2">
+		<input type="text" name="pole">
+		<input type="submit" name="button">
+	</form>
+
+</body>
+</html>
